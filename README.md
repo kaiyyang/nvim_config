00cbd07b1a0de30527ee00e7598680457d74b943
Kaiyuan Yang <yang.k.yuan@gmail.com>
2023-08-13 11:34:41 -0700

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 
 My own neovim config
+
+
+# Prerequisites
+- Neovim version >= 0.70
+- Install Packer.nvim
+```shell
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+```
--- init.lua
+++ init.lua
@@ -33,47 +33,47 @@ for i = 1, 10 do
 end
 
 -- Settings
-local opt = vim.opt
-opt.textwidth = 100
-opt.scrolloff = 7
-opt.wildignore = { '*.o', '*~', '*.pyc' }
-opt.wildmode = 'longest,full'
-opt.whichwrap:append '<,>,h,l'
-opt.inccommand = 'nosplit'
-opt.lazyredraw = true
-opt.showmatch = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.tabstop = 2
-opt.softtabstop = 0
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.number = true
-opt.relativenumber = true
-opt.smartindent = true
-opt.laststatus = 3
-opt.showmode = false
-opt.shada = [['20,<50,s10,h,/100]]
-opt.hidden = true
-opt.shortmess:append 'c'
-opt.joinspaces = false
-opt.guicursor = [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]]
-opt.updatetime = 100
-opt.conceallevel = 2
-opt.concealcursor = 'nc'
-opt.previewheight = 5
-opt.undofile = true
-opt.synmaxcol = 500
-opt.display = 'msgsep'
-opt.cursorline = true
-opt.modeline = false
-opt.mouse = 'nivh'
-opt.signcolumn = 'yes:1'
-opt.cmdheight = 0
-opt.splitbelow = true
-opt.splitright = true
-opt.timeoutlen = 200
-opt.fillchars = [[vert:│,horiz:─,eob: ]]
+ -- local opt = vim.opt
+ -- opt.textwidth = 100
+ -- opt.scrolloff = 7
+ -- opt.wildignore = { '*.o', '*~', '*.pyc' }
+ -- opt.wildmode = 'longest,full'
+ -- opt.whichwrap:append '<,>,h,l'
+ -- opt.inccommand = 'nosplit'
+ -- opt.lazyredraw = true
+ -- opt.showmatch = true
+ -- opt.ignorecase = true
+ -- opt.smartcase = true
+ -- opt.tabstop = 2
+ -- opt.softtabstop = 0
+ -- opt.expandtab = true
+ -- opt.shiftwidth = 2
+ -- opt.number = true
+ -- opt.relativenumber = true
+ -- opt.smartindent = true
+ -- opt.laststatus = 3
+ -- opt.showmode = false
+ -- opt.shada = [['20,<50,s10,h,/100]]
+ -- opt.hidden = true
+ -- opt.shortmess:append 'c'
+ -- opt.joinspaces = false
+ -- opt.guicursor = [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]]
+ -- opt.updatetime = 100
+ -- opt.conceallevel = 2
+ -- opt.concealcursor = 'nc'
+ -- opt.previewheight = 5
+ -- opt.undofile = true
+ -- opt.synmaxcol = 500
+ -- opt.display = 'msgsep'
+ -- opt.cursorline = true
+ -- opt.modeline = false
+ -- opt.mouse = 'nivh'
+ -- opt.signcolumn = 'yes:1'
+ -- opt.cmdheight = 0
+ -- opt.splitbelow = true
+ -- opt.splitright = true
+ -- opt.timeoutlen = 200
+ -- opt.fillchars = [[vert:│,horiz:─,eob: ]]
 
 -- Keybindings
 local silent = { silent = true, noremap = true }
